9a76b536817b2dda198442b18a70a4ffd8896cb0
Ricardo Russ <ricardoruss@live.com>
2020-04-09 14:57:10 -0400

    adding in basic file structure

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Kimchi Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Asteroid", false);
